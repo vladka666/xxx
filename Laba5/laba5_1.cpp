cfe6e917ca8a6d359eb30414ff8d2065461dc706
vladka666 <64436522+vladka666@users.noreply.github.com>
2020-04-30 20:56:44 +0300

    Update laba5_1.cpp

--- Laba5/laba5_1.cpp
+++ Laba5/laba5_1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main() {
-    double arr[10];
+    double arr[20];
     double sum = 0, average;
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 20; ++i) {
         std::cout << "Enter " << i + 1 << " num" << std::endl;
         std::cin >> arr[i];
         sum += arr[i];
     }
 
-    average = sum / 10;
+    average = sum / 20;
     std::cout
             << "Sum of array: " << sum << std::endl
             << "Average value is " << average << std::endl;
